76c2b08029f9681fb7164b2de7f6b7b82d6b55d4
Maria Koczwara <mkoczwara@poczta.onet.pl>
2026-03-05 12:39:37 +0100

    Fix routing in server file

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require('cors');
 const path = require('path');
+
 const siteRoute = require("./routes/sites");
 const regionRoute = require("./routes/regions");
 
 const app = express();
 
-app.get("/api/mapbox-events", async (req, res) => {
-    const url = `https://events.mapbox.com/events/v2?access_token=${process.env.REACT_APP_MAPBOX}`;
-    console.log('wyf', url)
-    const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
-});
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-});
-
-dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`${process.env.MONGO_URL}test`, {
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => {
-        console.log("db ok");
-    })
+    .then(() => console.log("db ok"))
     .catch((err) => console.log(err));
 
 app.use("/api/sites", siteRoute);
 app.use("/api/regions", regionRoute);
 
-app.listen(8000, () => {
-    console.log("is running....")
-})
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log("is running on ${PORT}")
+})
